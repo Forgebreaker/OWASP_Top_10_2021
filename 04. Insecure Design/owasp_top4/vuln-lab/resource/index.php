11efa7f2e7261372815a99d067a81f3ca949a9b4
Forgebreaker <hungthinhtran.office@gmail.com>
2024-03-24 19:31:34 +0700

    Update

--- 04. Insecure Design/owasp_top4/vuln-lab/resource/index.php
+++ 04. Insecure Design/owasp_top4/vuln-lab/resource/index.php
@@ -62,7 +62,7 @@ if (isset($_POST['id']) and isset($_POST['otp'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Insecure Design</title>
+    
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
