6aaf7f1e4562044567dd005cd5447c9dba6d1f95
Forgebreaker <hungthinhtran.office@gmail.com>
2024-03-26 15:36:53 +0700

    Update

--- 01. Broken Access Control/owasp_top1/vuln-lab/resource/account.php
+++ 01. Broken Access Control/owasp_top1/vuln-lab/resource/account.php
@@ -1,10 +1,10 @@
 <?php
 $db = new SQLite3('database.db');
 
-function getAccountDetails($db, $accountNumber) {
-    $stmt = $db->prepare('SELECT * FROM users WHERE account_number = :accountNumber');
-    $stmt->bindValue(':accountNumber', $accountNumber, SQLITE3_INTEGER);
-    $result = $stmt->execute();
+function account($db, $accountNumber) {
+    $connect = $db->prepare('SELECT * FROM users WHERE account_number = :accountNumber');
+    $connect->bindValue(':accountNumber', $accountNumber, SQLITE3_INTEGER);
+    $result = $connect->execute();
 
     return $result->fetchArray();
 }
@@ -12,7 +12,7 @@ function getAccountDetails($db, $accountNumber) {
 $accountDetails = null;
 if (isset($_GET['account_number'])) {
     $accountNumber = $_GET['account_number'];
-    $accountDetails = getAccountDetails($db, $accountNumber);
+    $accountDetails = account($db, $accountNumber);
 }
 include 'account.html';
 ?>
